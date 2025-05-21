7a979cc1749750d504347069264ebd87911c23e4
Ajay Nagar <anagarjnv889@gmail.com>
2025-05-21 15:46:03 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter Second number: "))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+fullname=firstname+" "+lastname+"!"
+print("Hello,",fullname,"Welcome to python program.")
